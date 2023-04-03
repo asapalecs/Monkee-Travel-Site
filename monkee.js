35558ff5e94d82820e536322c3b0581701c5f007
Alex <asapalecs@gmail.com>
2023-04-03 21:51:39 +0300

    Tests

--- monkee.js
+++ monkee.js
@@ -1,37 +1,38 @@
-const express = require('express')
-const expressHandlebars = require('express-handlebars')
-const app = express()
-const port = process.env.PORT || 3000
-const fortune = require('./lib/forune')
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const app = express();
+const port = process.env.PORT || 3000;
+const fortune = require("./lib/fortune");
+const handlers = require("./lib/handlers");
 
 // configure Handlebars view engine
-app.engine('handlebars', expressHandlebars.engine({
-  defaultLayout: 'main',
-}))
-app.set('view engine', 'handlebars')
+app.engine(
+  "handlebars",
+  expressHandlebars.engine({
+    defaultLayout: "main",
+  })
+);
+app.set("view engine", "handlebars");
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + "/public"));
 
+app.get("/", handlers.home);
 
-app.get('/', (req, res) => res.render('home'))
-
-app.get('/about', (req, res) => {
-  res.render('about', {fortune: randomQuote});  
-})
+app.get("/about", handlers.about);
 
 // custom 404 page
-app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
+app.use(handlers.notFound);
 
 // custom 500 page
-app.use((err, req, res, next) => {
-  console.error(err.message)
-  res.status(500)
-  res.render('500')
-})
+app.use(handlers.serverError);
 
-app.listen(port, () => console.log(
-  `Express started on http://localhost:${port}; ` +
-  `press Ctrl-C to terminate.`))
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(
+      `Express started on http://localhost:${port}` +
+        "; press Ctrl-C to terminate."
+    );
+  });
+} else {
+  module.exports = app;
+}
